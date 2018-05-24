160f24bab7cbf55a709aef1e6ee0749962aabc95
ROBO <robertas.bauras@gmail.com>
2018-05-24 22:43:22 +0300

    Initial

--- core/core.js
+++ core/core.js
@@ -1,4 +1,18 @@
+let pikos = [];
+
 
 module.exports.run = function(req, res, next){
-	res.send(`ok piko :)`);
+	if (req.url == '/'){
+		res.send({});
+	}
+	else if(req.url == '/create'){
+		res.send([
+					{name: 'Piko', healthStatus: 100, family: 0, clan: 0, country: 0, city: 0, gender: 'male', gold: 0, food: 0, food: 0, tools: 0},
+					
+					]);
+	}
+	else{
+		res.send(`Access deny.`);
+	}
 }
+
